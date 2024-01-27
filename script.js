dfb5047114cd62fcb6dcedcdc5d73c4010b62206
ilkinsufi <ilkinsufi@gmail.com>
2024-01-27 23:22:36 +0400

    "20. Valid Parentheses" solved

--- script.js
+++ script.js
@@ -32,3 +32,30 @@
 
 //   return firstStr.substring(0, i);
 // };
+
+// 20. Valid Parentheses
+// var isValid = function (s) {
+//   let stack = [];
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+//       stack.push(s[i]);
+//     } else if (stack.length === 0) {
+//       return false;
+//     } else {
+//       if (
+//         (s[i] === ")" && stack[stack.length - 1] === "(") ||
+//         (s[i] === "}" && stack[stack.length - 1] === "{") ||
+//         (s[i] === "]" && stack[stack.length - 1] === "[")
+//       ) {
+//         stack.pop();
+//       } else {
+//         return false; 
+//       }
+//     }
+//   }
+
+//   return stack.length === 0;
+// };
+
+// isValid("()[]{}");
