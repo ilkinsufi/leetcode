7e2541c2e151ccd7c686976d41b22e38da3b9bfe
ilkinsufi <ilkinsufi@gmail.com>
2025-09-20 22:46:17 +0400

    231. Power of Two

--- script.js
+++ script.js
@@ -165,11 +165,21 @@
 // var convertToTitle = function(columnNumber) {
 //     let title = '';
 //     while (columnNumber > 0) {
-//         columnNumber--;  
+//         columnNumber--;
 //         title = alphabet[columnNumber % 26] + title;
 //         columnNumber = Math.floor(columnNumber / 26);
 //     }
 //     return title;
 // };
-
 // convertToTitle(703);
+
+// 231. Power of Two
+// var isPowerOfTwo = function (n) {
+//   do {
+//     n /= 2;
+//   } while (n > 1);
+
+//   return n === 1 ? true : false;
+// };
+
+// isPowerOfTwo(32);
